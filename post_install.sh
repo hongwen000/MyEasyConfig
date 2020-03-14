a08790cd3a02831cd3e3239de2ed49437aa7a337
hongwen000 <hongwen000@outlook.com>
2020-03-14 23:14:32 +0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -170,7 +170,8 @@ GetNPMSoftware()
 
 GetGitSoftware()
 {
-	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	_ProxyEnv ~/.fzf/install --all
 }
 
 
